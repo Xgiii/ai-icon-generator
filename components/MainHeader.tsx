ac0726840ec92aecd80f3ee50014f753e5f33960
Filip Gurak <filip.gurak999@gmail.com>
2023-03-07 19:20:59 +0100

    auth

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -1,11 +1,12 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 
 function MainHeader() {
   const router = useRouter();
-  const user = null;
-
+  const { data: session } = useSession();
+  
   return (
     <div className='flex items-center justify-between p-2 md:pt-4 md:px-12 lg:px-16'>
       <ul className='flex items-center space-x-12'>
@@ -18,25 +19,31 @@ function MainHeader() {
         <li>
           <Link href='/generate'>Generate</Link>
         </li>
-        {user && (
+        {session?.user && (
           <li>
             <Link href='/collection'>Collection</Link>
           </li>
         )}
       </ul>
       <div className='flex items-center space-x-3'>
-        {!user && (
-          <button className='px-6 py-2 bg-gray-800 text-gray-400 font-semibold rounded-md uppercase'>
+        {!session?.user && (
+          <button
+            onClick={() => signIn('google')}
+            className='px-6 py-2 bg-gray-800 text-gray-400 font-semibold rounded-md uppercase'
+          >
             Sign In
           </button>
         )}
-        {user && (
+        {session?.user && (
           <>
             <p>999 credits left</p>
             <button className='px-6 py-2 bg-blue-400 text-gray-800 font-semibold rounded-md uppercase'>
               Buy Credits
             </button>
-            <button className='px-6 py-2 bg-gray-800 text-gray-400 font-semibold rounded-md uppercase'>
+            <button
+              onClick={() => signOut()}
+              className='px-6 py-2 bg-gray-800 text-gray-400 font-semibold rounded-md uppercase'
+            >
               Sign Out
             </button>
           </>
