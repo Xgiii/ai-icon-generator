292829e6dbd750ff29c7b390074ec74a50b2bbea
Filip Gurak <filip.gurak999@gmail.com>
2023-03-07 16:18:03 +0100

    main header

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -9,7 +9,10 @@ function MainHeader() {
   return (
     <div className='flex items-center justify-between p-2 md:pt-4 md:px-12 lg:px-16'>
       <ul className='flex items-center space-x-12'>
-        <h1 onClick={() => router.push('/')} className='font-bold text-2xl cursor-pointer'>
+        <h1
+          onClick={() => router.push('/')}
+          className='font-bold text-2xl cursor-pointer'
+        >
           AI Icon Generator
         </h1>
         <li>
@@ -21,9 +24,24 @@ function MainHeader() {
           </li>
         )}
       </ul>
-      <button className='px-6 py-2 bg-gray-800 text-gray-400 font-semibold rounded-md uppercase'>
-        Sign In
-      </button>
+      <div className='flex items-center space-x-3'>
+        {!user && (
+          <button className='px-6 py-2 bg-gray-800 text-gray-400 font-semibold rounded-md uppercase'>
+            Sign In
+          </button>
+        )}
+        {user && (
+          <>
+            <p>999 credits left</p>
+            <button className='px-6 py-2 bg-blue-400 text-gray-800 font-semibold rounded-md uppercase'>
+              Buy Credits
+            </button>
+            <button className='px-6 py-2 bg-gray-800 text-gray-400 font-semibold rounded-md uppercase'>
+              Sign Out
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
